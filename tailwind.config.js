7c02846a1f91d8fd2c447b2c91baafa781d586a0
Nandesh <nandesh.dev@gmail.com>
2024-03-25 15:24:01 +0530

    setup initial react with tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.jsx"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        "gray-100": '#13151B',
+        "gray-200": '#171821',
+        "gray-300": '#21222D',
+        "gray-400": "#262733",
+        "gray-500": '#2B2C39',
+        "gray-700": '#585A7C',
+        "gray-800": '#A9ABC2',
+        "white": '#FFFFFF'
+      },
+    },
+  },
+  plugins: [],
+}
+
